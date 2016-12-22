7f557c3a3695df8d9322bb4dd388413e5d76bd69
zgc <919506309@qq.com>
2016-12-22 14:52:03 +0800

    httpsClient

--- src/cn/zgc/socket/book/ch15/HTTPSClientWithListener.java
+++ src/cn/zgc/socket/book/ch15/HTTPSClientWithListener.java
@@ -4,33 +4,34 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.net.ssl.HandshakeCompletedListener;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+
 /**
- * 访问HTTPS站点,运行速度较http站点要慢
+ * 通过握手完成监听（HandshakeCompletedListener）输出SSL握手过程产生的一些信息
+ * @author gczhang
  */
-public class HTTPSClient {
+public class HTTPSClientWithListener {
+	private SSLSocket sslSocket;
 	SSLSocketFactory factory;
-	SSLSocket sslSocket;
-	String host = "www.baidu.com";
+	String host = "www.douyu.com"; 
 	
-	public HTTPSClient() throws IOException{
+	public void createSocket() throws Exception{
 		factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 		sslSocket = (SSLSocket) factory.createSocket(host, 443);
-		// 返回支持的加密套件
-		String[] scs = sslSocket.getSupportedCipherSuites();
-		// 返回支持的加密传输协议
-		String[] sp = sslSocket.getSupportedProtocols();
-		// 切换不同的https站点，输出同样。一般情况下通信双方的加密套件相同
-		System.out.println(scs.length+":"+Arrays.toString(scs));
-		System.out.println(sp.length+":"+Arrays.toString(sp));
-		
-		/* 为客户端设置不同的加密套件（默认情况下，服务端不使用这些套件），会导致握手失败（SSL握手过程中会协商实际使用的加密套件）
-		 * String[] strongSuits = {"SSL_DES_DSS_WITH_3DES_EDE_CBC_SHA",
-				"SSL_RSA_WITH_RC4_128_MD5","SSL_RSA_WITH_RC4_128_SHA",
-				"SSL_RSA_WITH_3DES_EDE_CBC_SHA"};
-		sslSocket.setEnabledCipherSuites(strongSuits);*/
+		sslSocket.addHandshakeCompletedListener(new HandshakeCompletedListener() {
+			
+			@Override
+			public void handshakeCompleted(HandshakeCompletedEvent event) {
+				String cs = event.getCipherSuite();
+				String remote = event.getSession().getPeerHost();
+				System.out.println("加密套件为:"+cs);
+				System.out.println("访问主机为:"+remote);
+			}
+		});
 	}
 	
 	public void communicate(){
@@ -49,7 +50,7 @@ public class HTTPSClient {
 			int len = -1;
 			while((len = in.read(buffer))!=-1){
 				baos.write(buffer, 0, len);
-				System.out.println(baos.toString());
+				//System.out.println(baos.toString());
 				baos.reset();
 			}
 		} catch (IOException e) {
@@ -63,7 +64,9 @@ public class HTTPSClient {
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
-		new HTTPSClient().communicate();
+	public static void main(String[] args) throws Exception {
+		HTTPSClientWithListener client = new HTTPSClientWithListener();
+		client.createSocket();
+		client.communicate();
 	}
 }
